8edc851a83714930c119f03cf9ba8a3ec974d658
its-sarath28 <sarathrs220@gmail.com>
2024-04-01 23:04:50 +0530

    Updates

--- server/server.js
+++ server/server.js
@@ -33,7 +33,14 @@ app.use(express.static(path.join(__dirname, "/server/public")));
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ origin: true }));
+// app.use(cors({ origin: true }));
+app.use(
+  cors({
+    origin: "https://movie-booking-site-two.vercel.app",
+    methods: ["POST", "PUT", "GET", "DELETE"],
+    credentials: true,
+  })
+);
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/movies", movieRouter);
