5699c7fecdf08afca5b48e41650a14f31098a530
its-sarath28 <sarathrs220@gmail.com>
2024-04-05 10:02:13 +0530

    Updates

--- server/server.js
+++ server/server.js
@@ -34,7 +34,14 @@ app.use(express.static(path.join(__dirname, "/server/public")));
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+// app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    methods: ["POST", "GET", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/movies", movieRouter);
